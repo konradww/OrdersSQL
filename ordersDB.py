49bb0dd8152c4edf3324d4455eb6f2a7b785eabc
Konrad <marek.dudek@protonmail.ch>
2018-05-29 20:24:54 +0200

    i added two method addCustomer and addProduct

--- ordersDB.py
+++ ordersDB.py
@@ -31,3 +31,29 @@ cursor = cnxn.cursor()
 # cursor.execute("select * from dbo.cust")
 # for row in cursor.fetchall():
 #     logging.debug(row)
+# cursor.execute("select * from dbo.product")
+# cursor.execute("insert into dbo.product(nameProduct) values ('tomato')")
+
+class shop(object):
+    def addproduct(self, nameproduct):
+        question = ("IF NOT EXISTS (SELECT * FROM dbo.product WHERE nameProduct like '%s')"
+                    "BEGIN "
+                    "insert into dbo.product(nameProduct) values ('%s') "
+                    "END" % (nameproduct, nameproduct))
+        return question
+
+
+    def addcustomer(self, Company, ContactName, Address, City, Telephone, Mail, NIP):
+        question = ("IF NOT EXISTS (SELECT * FROM dbo.customer WHERE NIP = %i) "
+                    "BEGIN "
+                    "insert into dbo.customer(Company, ContactName, Address, City, Telephone, Mail, NIP) "
+                    "values ('%s', '%s', '%s', '%s', '%s', '%s', %i) "
+                    "END" % (NIP, Company, ContactName, Address, City, Telephone, Mail, NIP))
+        return question
+
+
+obj = shop()
+cursor.execute(obj.addproduct('tomato'))
+cursor.commit()
+cursor.execute(obj.addcustomer('Trek', 'Edward', 'Gromska 15', 'Gdynia', '604500700', 'edwardgromski@gmail.com', 1111))
+cursor.commit()
